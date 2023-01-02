240f156be0a10c4ec1c45904acb4ad85ba7c2020
enesozmus <ensozmus@gmail.com>
2023-01-02 18:00:50 +0300

    Lesson38 | Complex Query Operators - Inner, Left, Right, Full, Cross Joins

---
+++ src/Lesson38/Practice/Program.cs
@@ -0,0 +1,248 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+#region Join() - INNER JOIN
+#region Method Syntax
+AppDbContext _context = new();
+var result = await _context.Categories
+                            .Join
+                            (
+                              // kimi join yapmak istiyorsun
+                              _context.Products,
+                                // bağlantılar
+                                category => category.Id,
+                                product => product.CategoryId,
+                              (category, product) => new
+                              {
+                                // oluşturmak istediğin yeni liste
+                                CategoryName = category.Name,
+                                ProductName = product.Name,
+                                ProductPrice = product.Price,
+                                KontrolSende = product.Barcode
+                              }
+                            ).ToListAsync();
+// Triple
+var result3 = await _context.Categories
+                              .Join
+                              (
+                                // kimi join yapmak istiyorsun
+                                _context.Products,
+                                  category => category.Id,
+                                  product => product.CategoryId,
+                                (category, product) => new
+                                // aşağidaki temsili up
+                                { category, product }
+                              )
+                              .Join
+                              (
+                                // kimi join yapmak istiyorsun
+                                _context.ProductFeatures,
+                                  up => up.product.Id,
+                                  productFeature => productFeature.Id,
+                                (up, productFeature) => new
+                                {
+                                  CategoryName = up.category.Name,
+                                  ProductName = up.product.Name,
+                                  ProductPrice = up.product.Price,
+                                  ProductColor = productFeature.Color
+                                }
+                              ).ToListAsync();
+#endregion
+
+#region Query Syntax
+var result2 = from category in _context.Categories
+              join product in _context.Products on category.Id equals product.CategoryId
+              select new
+              {
+                CategoryName = category.Name,
+                ProductName = product.Name,
+                ProductPrice = product.Price,
+                KontrolSende = product.Barcode
+              };
+// Triple
+var result4 = await (from category in _context.Categories
+                     join product in _context.Products on category.Id equals product.CategoryId
+                     join productFeature in _context.ProductFeatures on product.Id equals productFeature.Id
+                     select new
+                     {
+                       CategoryName = category.Name,
+                       ProductName = product.Name,
+                       ProductPrice = product.Price,
+                       ProductColor = productFeature.Color
+                     }).ToListAsync();
+#endregion
+#endregion
+
+
+#region from...join - LEFt JOIN & RIGHT JOIN
+// özünde → Kendisine bir üründetay tablosu bağlanmamış ürünleri göreceğiz
+// bir üründetay tablosu olanlar
+var result5 = await (from product in _context.Products
+                     join productFeature in _context.ProductFeatures on product.Id equals productFeature.Id
+                     select new { product }).ToListAsync();
+// DefaultIfEmpty(): boşsa default'unu al
+// özünde → Kendisine bir üründetay tablosu bağlanmış ve bağlanmamış ürünleri göreceğiz
+var result6 = await (from product in _context.Products
+                     join productFeature in _context.ProductFeatures on product.Id equals productFeature.Id into productFeatureList
+                     from productFeature in productFeatureList.DefaultIfEmpty()
+                     select new { product }).ToListAsync();
+// özünde → Kendisine bir üründetay tablosu bağlanmış ve bağlanmamış ürünleri + bağlı üründetay tablolarını + olmayanlar için null göreceğiz
+var result7 = await (from product in _context.Products
+                     join productFeature in _context.ProductFeatures on product.Id equals productFeature.Id into productFeatureList
+                     from productFeature in productFeatureList.DefaultIfEmpty()
+                     select new { product, productFeature }).ToListAsync();
+
+var leftJoinResult = await (from product in _context.Products
+                            join productFeature in _context.ProductFeatures on product.Id equals productFeature.Id
+                            into productFeatureList
+                            from productFeature in productFeatureList.DefaultIfEmpty()
+                            select new
+                            {
+                              ProductName = product.Name,
+                              ProductPrice = product.Price,
+                              ProductColor = productFeature.Color,
+                              ProductWidth = (int?)productFeature.Width,
+                              ProductHeight = (int?)productFeature.Height == null ? 0 : productFeature.Height
+                            }).ToListAsync();
+
+var rightJoinResult = await (from productFeature in _context.ProductFeatures
+                             join product in _context.Products on productFeature.Id equals product.Id
+                             into productList
+                             from product in productList.DefaultIfEmpty()
+                             select new
+                             {
+                               ProductName = product.Name,
+                               ProductPrice = (decimal?)product.Price,
+                               ProductColor = productFeature.Color,
+                               ProductWidth = productFeature.Width,
+                               ProductHeight = productFeature.Height
+                             }).ToListAsync();
+#endregion
+
+
+#region Full Outer Join
+// cross + ürün + detay
+var left = await (from product in _context.Products
+                  join productFeature in _context.ProductFeatures on product.Id equals productFeature.Id
+                  into productFeatureList
+                  from productFeature in productFeatureList.DefaultIfEmpty()
+                  select new
+                  {
+                    ProductId = product.Id,
+                    ProductName = product.Name,
+                    ProductColor = productFeature.Color,
+                  }
+                  ).ToListAsync();
+var right = await (from productFeature in _context.ProductFeatures
+                   join product in _context.Products on productFeature.Id equals product.Id
+                   into productList
+                   from product in productList.DefaultIfEmpty()
+                   select new
+                   {
+                     ProductId = product.Id,
+                     ProductName = product.Name,
+                     ProductColor = productFeature.Color,
+                   }
+                  ).ToListAsync();
+
+var full_join = left.Union(right);
+Console.WriteLine("over");
+#endregion
+
+
+#region Seed Datas
+// AppDbContext _context = new();
+// await _context.AddAsync<Category>(new() { Name = "A - Category" });
+// await _context.AddAsync<Category>(new() { Name = "B - Category" });
+// await _context.AddAsync<Category>(new() { Name = "C - Category" });
+// await _context.AddAsync<Category>(new() { Name = "D - Category" });
+// await _context.SaveChangesAsync();
+// Console.WriteLine("over");
+
+
+// await _context.AddAsync<Product>(new() { CategoryId = 1, Name = "kalem 1", Price = 100, Stock = 200, Barcode = 123, ProductFeature = new ProductFeature() { Color = "Red", Height = 200, Width = 100 } });
+// await _context.AddAsync<Product>(new() { CategoryId = 1, Name = "kalem 2", Price = 100, Stock = 200, Barcode = 123, ProductFeature = new ProductFeature() { Color = "Red", Height = 200, Width = 100 } });
+// await _context.AddAsync<Product>(new() { CategoryId = 2, Name = "kalem 3", Price = 100, Stock = 200, Barcode = 123, ProductFeature = new ProductFeature() { Color = "Red", Height = 200, Width = 100 } });
+// await _context.AddAsync<Product>(new() { CategoryId = 1, Name = "kalem 4", Price = 100, Stock = 200, Barcode = 123 });
+// await _context.AddAsync<Product>(new() { CategoryId = 1, Name = "kalem 5", Price = 100, Stock = 200, Barcode = 123 });
+// await _context.SaveChangesAsync();
+// Console.WriteLine("over");
+
+#endregion
+
+
+#region Entites
+public class Category
+{
+  public Category()
+  {
+    Products = new HashSet<Product>();
+  }
+  public int Id { get; set; }
+  public string Name { get; set; }
+
+  public ICollection<Product> Products { get; set; }
+}
+public class Product
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  // #######.##
+  [Precision(9, 2)]
+  public decimal Price { get; set; }
+
+  [Precision(9, 2)]
+  public decimal DiscountPrice { get; set; }
+
+  public int Stock { get; set; }
+
+  public int Barcode { get; set; }
+  public bool IsDeleted { get; set; }
+  public int CategoryId { get; set; }
+  public Category Category { get; set; }
+
+  public ProductFeature ProductFeature { get; set; }
+}
+public class ProductFeature
+{
+  [Key, ForeignKey("Product")]
+  public int Id { get; set; }
+
+  public int Width { get; set; }
+  public int Height { get; set; }
+  public string Color { get; set; }
+
+  public Product Product { get; set; }
+}
+#endregion
+
+
+#region DbContext
+public class AppDbContext : DbContext
+{
+  public AppDbContext() { }
+
+  public DbSet<Product> Products { get; set; }
+  public DbSet<Category> Categories { get; set; }
+  public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.UseSqlServer("Data Source=DESKTOP-OPFJQHD; Database=JoinDb; Integrated Security=True;");
+  }
+}
+#endregion
+
+
+#region packages and commands
+// dotnet add package Microsoft.EntityFrameworkCore --version 6.0.12
+// dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 6.0.12
+// dotnet add package Microsoft.EntityFrameworkCore.Tools --version 6.0.12
+// dotnet add package Microsoft.EntityFrameworkCore.Design --version 6.0.12
+
+// dotnet ef migrations add mig_1
+// dotnet ef database update
+#endregion
---
+++ src/Lesson38/Program.cs
@@ -0,0 +1,245 @@
+﻿Console.WriteLine("Hello, World!");
+
+#region Complex Query Operators
+/**
+
+    * * Language Integrated Query (LINQ) contains many complex operators, which combine multiple data sources or does complex processing.
+    * ! Not all LINQ operators have suitable translations on the server side. 
+    * * Sometimes, a query in one form translates to the server but if written in a different form doesn't translate even if the result is the same.
+    * * A particular query, which is translated in SqlServer, may not work for SQLite databases.   
+    * ? DefaultIfEmpty() : Sorgulama sürecinde ilişkisel olarak karşılığı olmayan verilere default değerini yazdıran bir fonksiyondur.
+ 
+*/
+#endregion
+
+
+#region Join() → SQL [Inner Join]
+/**
+
+    * * The LINQ Join operator allows you to connect two data sources based on the key selector for each source, generating a tuple of values when the key matches.
+    * * It naturally translates to INNER JOIN on relational databases. 
+
+*/
+/** Example 1
+    * * PersonPhoto & Person:
+
+    * * query syntax
+        var query = from photo in context.Set<PersonPhoto>()
+                    join person in context.Set<Person>()
+                        on photo.PersonPhotoId equals person.PhotoId
+                    select new { person, photo };
+                    // or
+                    select new { person.Name, photo.Url }; 
+        // query.ToList();
+
+    * * method syntax
+        var query = context.Photos.Join
+                            (
+                                context.Persons,
+                                photo => photo.PersonId, person => person.PersonId,
+                            (photo, person) => new
+                            {
+                                person.Name,
+                                photo.Url
+                            });
+*/
+/** Example 2
+
+    * * The following query joins Person and EmailAddresses table using the join Query operator.
+    * * The Join operator uses the 'Equals Keyword' to compare the specified properties.
+    * * The query begins with from p in db.People which is the outer table in our join.
+    * * We then use the 'join keyword' to join the inner table. (join e in db.EmailAddresses)
+    * * The 'on keyword' is used to specify the join condition.
+    * * The first part of the condition should always from the outer table (i.e People).
+    * ! We use the 'equals keyword' to compare the conditions. Also, we can only compare for equality. Other comparisons are not supported.
+
+        using (AdventureWorks _context = new AdventureWorks())
+        {
+            var person = (from p in _context.People
+                            join e in _context.EmailAddresses
+                            on p.BusinessEntityID equals e.BusinessEntityID
+                            where p.FirstName == "KEN"
+                        select new
+                        {
+                            ID = p.BusinessEntityID, 
+                            FirstName = p.FirstName, 
+                            MiddleName = p.MiddleName, 
+                            LastName = p.LastName, 
+                            EmailID = e.EmailAddress1 
+                        }).ToList();
+
+            var person = _context.People.Join
+                        (
+                            _context.EmailAddresses, 
+                            p => p.BusinessEntityID, e => e.BusinessEntityID,
+                            (p, e) => new { 
+                                FirstName = p.FirstName, 
+                                MiddleName = p.MiddleName, 
+                                LastName = p.LastName, 
+                                EmailID = e.EmailAddress1 }
+                        ).Take(5);
+        }
+*/
+#endregion
+
+
+#region Join() → SQL [Inner Join] - on Multiple Columns
+/**
+
+    * * To use join on more than one columns (Join by using composite keys), we need to define an anonymous type with the values we want to compare
+    * * Query Syntax
+
+        var query = from photo in _context.Photos
+                    join person in _context.Persons
+                        on new { photo.PersonId, photo.Url } equals new { person.PersonId, Url = person.Name }
+                    select new
+                    {
+                        person.Name,
+                        photo.Url
+                    };
+
+    * ! works only if  the data types and the names of the properties in the anonymous types match
+
+        var result = (from m1 in db.model1
+              join m2 in db.model2
+               on new { m1.field1 , m1.field2 } 
+               equals new { m2.field1, m2.field2 }
+             where m1.FirstName == "KEN"
+             select new
+              {
+                field1 = m1.field1,
+                field2 = m1.field2,
+                someField = m2.someField
+             }).ToList();
+
+    * * Method Syntax
+
+        var query = context.Photos .Join
+                            (
+                                context.Persons,
+                                photo => new { photo.PersonId, photo.Url },
+                                person => new { person.PersonId, Url = person.Name },
+                            (photo, person) => new
+                            {
+                                person.Name,
+                                photo.Url
+                            });
+*/
+#endregion
+
+
+#region Group Join
+//var query = from person in context.Persons
+//            join order in context.Orders
+//                on person.PersonId equals order.PersonId into personOrders
+//            //from order in personOrders
+//            select new
+//            {
+//                person.Name,
+//                Count = personOrders.Count(),
+//                personOrders
+//            };
+#endregion
+
+#region Left Join
+//var query = from person in context.Persons
+//            join order in context.Orders
+//                on person.PersonId equals order.PersonId into personOrders
+//            from order in personOrders.DefaultIfEmpty()
+//            select new
+//            {
+//                person.Name,
+//                order.Description
+//            };
+
+//var datas = await query.ToListAsync();
+#endregion
+
+#region Right Join
+//var query = from order in context.Orders
+//            join person in context.Persons
+//                on order.PersonId equals person.PersonId into orderPersons
+//            from person in orderPersons.DefaultIfEmpty()
+//            select new
+//            {
+//                person.Name,
+//                order.Description
+//            };
+
+//var datas = await query.ToListAsync();
+#endregion
+
+#region Full Join
+//var leftQuery = from person in context.Persons
+//                join order in context.Orders
+//                    on person.PersonId equals order.PersonId into personOrders
+//                from order in personOrders.DefaultIfEmpty()
+//                select new
+//                {
+//                    person.Name,
+//                    order.Description
+//                };
+
+
+//var rightQuery = from order in context.Orders
+//                 join person in context.Persons
+//                     on order.PersonId equals person.PersonId into orderPersons
+//                 from person in orderPersons.DefaultIfEmpty()
+//                 select new
+//                 {
+//                     person.Name,
+//                     order.Description
+//                 };
+
+// var fullJoin = leftQuery.Union(rightQuery);
+// var datas = await fullJoin.ToListAsync();
+#endregion
+
+#region Cross Join  
+// from ... from
+//var query = from order in context.Orders
+//            from person in context.Persons
+//            select new
+//            {
+//                order,
+//                person
+//            };
+
+//var datas = await query.ToListAsync();
+#endregion
+
+#region Collection Selector'da Where Kullanma Durumu
+//var query = from order in context.Orders
+//            from person in context.Persons.Where(p => p.PersonId == order.PersonId)
+//            select new
+//            {
+//                order,
+//                person
+//            };
+
+//var datas = await query.ToListAsync();
+#endregion
+
+#region Cross Apply
+//var query = from person in context.Persons
+//            from order in context.Orders.Select(o => person.Name)
+//            select new
+//            {
+//                person,
+//                order
+//            };
+
+//var datas = await query.ToListAsync();
+#endregion
+
+#region Outer Apply
+//var query = from person in context.Persons
+//            from order in context.Orders.Select(o => person.Name).DefaultIfEmpty()
+//            select new
+//            {
+//                person,
+//                order
+//            };
+
+//var datas = await query.ToListAsync();
+#endregion
